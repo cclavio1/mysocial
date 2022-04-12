1009cc01e2fb0acdb18cd3b82eb73f0a46688382
cclavio1 <cfcrazer@gmail.com>
2022-04-12 15:09:45 +0800

    Test posts props

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,7 +1,12 @@
 
 
-export default function Posts(){
+export default function Posts(props){
     return(
         <h1>this will display posts</h1>
     )
+}
+
+
+Posts.defaultProps = {
+    posts:"empty"
 }
