b260dd7e63188f21ac36de2b652d5a6c19ca0bdc
cclavio1 <cfcrazer@gmail.com>
2022-04-12 15:12:13 +0800

    Reference posts component to start page

--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -1,9 +1,11 @@
 import NavBar from "../Components/NavBar"
+import Posts from "../Components/Posts"
 
 export default function Start(){
     return(
         <div>
         <NavBar/>
+        <Posts />
           <h1>My Social</h1>
           
         </div>
