6335caf5c97836388f97a59a7f65a95b95e3d85b
cclavio1 <cfcrazer@gmail.com>
2022-04-12 11:17:45 +0800

    Add navbar component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+
+
+export default function NavBar(){
+    return(
+        <div>
+            <ul>
+                <ol>
+                    <a>Sample Links</a>
+                </ol>
+            </ul>
+        </div>
+    )
+}
