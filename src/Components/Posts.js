73eb57fc1dcd0f3bf53bec9538854d105a8c532b
cclavio1 <cfcrazer@gmail.com>
2022-04-14 14:12:44 +0800

    Provide more details on post

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react"
 
 
 export default function Posts(props){
+    let [posts,setPosts]=useState(<div className="text-center">
+    <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"></img>
+</div>)
     return(
-        <h1>this will display posts</h1>
+        <div>
+            <h1>this will display posts</h1>
+            {posts}
+        </div>
     )
 }
 
