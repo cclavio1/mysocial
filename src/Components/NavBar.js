688f1f6404a7dcf4308072347c82ff2a09caff28
cclavio1 <cfcrazer@gmail.com>
2022-04-14 13:56:15 +0800

    Add some details on navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,13 +2,17 @@ import logo from './../MySocialLogo.png'
 
 export default function NavBar(){
     return(
-        <div>
+        <div className='sticky-top' style={{
+            background: "rgb(8,0,143)",
+            background: "linear-gradient(90deg, rgba(8,0,143,1) 0%, rgba(0,153,184,1) 25%, rgba(68,68,209,0.8244485294117647) 50%)",
+            color:"white"
+        }}>
             <ul className='d-flex justify-content-end align-items-center'>
                 <img src={logo} className="img-fluid rounded-circle" style={{ width:"75px"
             }}></img> <h1>Social</h1>
                 <ol className='mx-auto'>
-                    <a className='nav-links mx-5'>Sample Links1</a>
-                    <a className='nav-links'>Sample Links2</a>
+                    <a className='nav-links mx-5 text-light'>Sample Links1</a>
+                    <a className='nav-links text-light'>Sample Links2</a>
                 </ol>
             </ul>
         </div>
--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -3,9 +3,10 @@ import Posts from "../Components/Posts"
 
 export default function Start(){
     return(
-        <div>
+        <div style={{backgroundImage:"url(https://www.freevector.com/uploads/vector/preview/30546/Pastel_Background_vector_5.jpg)",minHeight:"100vh"}}>
         <NavBar/>
         <Posts />
+
           <h1>My Social</h1>
           
         </div>
