218f9293173bd9fc2d01f653e538fcab0d2430f3
cclavio1 <cfcrazer@gmail.com>
2022-04-14 14:00:06 +0800

    Structured main page layout

--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-bootstrap"
 import NavBar from "../Components/NavBar"
 import Posts from "../Components/Posts"
 
@@ -5,9 +6,17 @@ export default function Start(){
     return(
         <div style={{backgroundImage:"url(https://www.freevector.com/uploads/vector/preview/30546/Pastel_Background_vector_5.jpg)",minHeight:"100vh"}}>
         <NavBar/>
-        <Posts />
-
-          <h1>My Social</h1>
+        <Container>
+          <Row>
+            <Col xs={6}>
+              <Posts />
+            </Col>
+            <Col xs={6}>
+              <Posts />
+            </Col>
+          </Row>
+        </Container>
+        
           
         </div>
     )
