2aad0577112d300918821b8d503bd123565d5b4c
cclavio1 <cfcrazer@gmail.com>
2022-04-14 14:02:47 +0800

    Add profile more info props

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,1 +1,14 @@
 
+
+
+export default function Profile(props){
+    return(
+        <h1>this will display profile</h1>
+    )
+}
+
+
+Profile.defaultProps = {
+    username:null,
+    imgurl:"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+}
