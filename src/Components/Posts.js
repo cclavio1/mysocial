dc02e47074c3db750d23834f72f31e8c643763cc
cclavio1 <cfcrazer@gmail.com>
2022-04-16 18:20:20 +0800

    Add useEffect for posts retrieve

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -5,6 +5,10 @@ export default function Posts(props){
     let [posts,setPosts]=useState(<div className="text-center">
     <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"></img>
 </div>)
+
+useEffect(()=>{
+    //fetch posts here
+},[])
     return(
         <div>
             <h1>Loading Posts</h1>
